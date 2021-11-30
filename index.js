5ea013a45a30bd5e237b1130500e8965ed5ebaa5
Lokesh-Kanna <lokeshkanna.work@gmail.com>
2021-11-30 14:45:43 +0530

    Added express, nodemon. Changed package settings to need

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 9000;
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+})
+
+app.listen(PORT, () => console.log("The server has started in port", PORT));
